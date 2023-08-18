04d3b194f8103f9080eebe80f103c72c9ef3f518
Maximilian <maximilian.muschalik@gmail.com>
2023-08-18 20:24:22 +0200

    changed run_time plotting

--- plot_runtime.py
+++ plot_runtime.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    file_name: str = "run_time.csv"
+    file_name: str = "run_time/run_time.csv"
+    depth = 25
+    file_name_interaction: str = f"run_time_interaction_{depth}.csv"
+
 
     col_names = ["model_id", "n_features", "interaction_order", "depth", "n_nodes",
                  "n_decision_nodes", "n_leaves", "leaves_times_depth", "elapsed_time"]
@@ -110,35 +113,7 @@ if __name__ == "__main__":
     ax.legend()
     plt.show()
 
-    plot_df = run_time_df[run_time_df["depth"] == 14]
-
-    x_axis_feature = "interaction_order"
-    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-    for model_id in plot_df["model_id"].unique():
-        model_df = plot_df[plot_df["model_id"] == model_id]
-        ax.errorbar(
-            model_df[x_axis_feature], model_df["elapsed_time_mean"],
-            yerr=model_df["elapsed_time_std"],
-            label=model_id, marker="o"
-        )
-        # add std as confidence band
-        ax.fill_between(
-            model_df[x_axis_feature],
-            model_df["elapsed_time_mean"] - model_df["elapsed_time_std"],
-            model_df["elapsed_time_mean"] + model_df["elapsed_time_std"],
-            alpha=0.2
-        )
-    ax.set_xlabel("interaction order")
-    ax.set_ylabel("Elapsed Time (s)")
-    ax.legend()
-    plt.show()
-
-    file_name: str = "run_time_interaction.csv"
-
-    col_names = ["model_id", "n_features", "interaction_order", "depth", "n_nodes",
-                 "n_decision_nodes", "n_leaves", "leaves_times_depth", "elapsed_time"]
-
-    run_time_df = pd.read_csv(file_name)
+    run_time_df = pd.read_csv(file_name_interaction)
 
     # get mean and std for each max_interaction_order of elapsed time
     run_time_df = run_time_df.groupby(
@@ -151,7 +126,7 @@ if __name__ == "__main__":
     new_col_names = col_names[:-1] + ["elapsed_time_mean", "elapsed_time_std"]
     run_time_df.columns = copy.deepcopy(new_col_names)
 
-    plot_df = run_time_df[run_time_df["depth"] == 15]
+    plot_df = run_time_df[run_time_df["depth"] == depth]
 
     x_axis_feature = "interaction_order"
     fig, ax = plt.subplots(1, 1, figsize=(10, 5))
